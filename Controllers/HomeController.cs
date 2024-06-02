a4f09986de58cbe62042f32441267b0314e4163b
swap5335 <swapnil21.huddar@gmail.com>
2024-06-02 14:42:28 +0530

    UI - Doctors Menu - Team Profile page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace StellarHospitalWebsite.Controllers
         {
             return View();
         }
+        public IActionResult Team()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
